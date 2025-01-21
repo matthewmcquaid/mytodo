409e4a62a9c69f1b2ad63a5d7240eeb8b0bf03e3
Matthew McQuaid <matthew.mcquaid@gmail.com>
2025-01-20 23:00:13 -0500

    adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ItemsList from './components/ItemsList';
+
+function App() {
+  return (
+    <div className="App">
+      <ItemsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ItemsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemsList = () => {
+    return (
+        <div>
+        <h1>Items List</h1>
+        </div>
+    );
+}
+
+export default ItemsList;
